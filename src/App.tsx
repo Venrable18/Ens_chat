b2e81523002f3aafcce3ead329751967a4cd4eed
Venrable18 <onenvictor@gmail.com>
2025-09-16 10:51:08 +0100

    experimenting

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import ChatDashboard from "./ChatDashboard/Dashboard.js";
+// import ChatDashboard from "./ChatDashboard/Dashboard.js";
 import AppLayout from "./Components/Layout/index.js";
  import Registration from "./Components/RegistrationForm/Registration.js";
 
